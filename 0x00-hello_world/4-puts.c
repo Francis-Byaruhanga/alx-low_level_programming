aa906aea8952cc4101e41e303b92a7026b8c95ea
Francis Byaruhanga <byaruhangafrancis23@gmail.com>
2023-07-14 08:54:26 -0700

    Successful Program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,7 @@
-#include <studio.h>
+#include <stdio.h>
 
-/**
- * main - this is the main function
- * Return: Always 0
-*/
 int main(void) 
 {
-	puts('\"'Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
