71954e2733b04a9f95be67928b41d54fda9d3f5f
Francis Byaruhanga <byaruhangafrancis23@gmail.com>
2023-07-14 12:39:28 -0700

    Finally, C runs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main(void) 
+/**
+ * Function main - this is the entry point for the program
+ * Puts - prints statement as is
+ * Return: always 0 (success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
