a21c6bfc0a38535284397c6f36824fbb8cddaef1
Francis Byaruhanga <byaruhangafrancis23@gmail.com>
2023-07-17 10:22:52 -0700

    Prints all the letters except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point for the program.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		if (c != 101 && c != 113)
+		{
+			putchar(c);
+			putchar('\n');
+		}
+	}
+
+	return (0);
+}
