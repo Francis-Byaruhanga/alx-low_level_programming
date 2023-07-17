502d282fe41613b9ecb1e4f4c18c496249c24bcc
Francis Byaruhanga <byaruhangafrancis23@gmail.com>
2023-07-17 11:48:00 -0700

    Print lines

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		putchar('\n');
+	}
+
+	return (0);
+}
